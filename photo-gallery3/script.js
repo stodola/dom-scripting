39e9191a50792eaa82e5930af50d3a118527a139
stodola <florian.winski@interia.pl>
2017-12-26 11:52:03 +0100

    Create script.js

---
+++ photo-gallery3/script.js
@@ -0,0 +1,30 @@
+function prepareGallery() {
+if (!document.getElementsByTagName) return false;
+if (!document.getElementById) return false;
+if (!document.getElementById("imagegallery")) return false;
+	
+	var gallery = document.getElementById("imagegallery");
+	var links = gallery.getElementsByTagName("a");
+for ( var i=0; i < links.length; i++) {
+	links[i].onclick = function() {
+	return  !showPic(this);
+
+}
+}
+}
+
+function showPic(whichpic) {
+if (!document.getElementById("placeholder")) return false;
+var source = whichpic.getAttribute("href");
+var placeholder = document.getElementById("placeholder");
+placeholder.setAttribute("src",source);
+if (document.getElementById("description")) {
+var text = whichpic.getAttribute("title");
+var description = document.getElementById("description");
+description.firstChild.nodeValue = text;
+}
+return true;
+}
+
+window.onload = prepareGallery;
+
